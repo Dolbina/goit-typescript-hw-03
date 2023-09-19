33e95d79b760f1f1ca80812b2ad920a5eca2298d
Ann Dolbina <dolbinaann@gmail.com>
2023-09-19 22:16:44 +0300

    add hw 1,2,3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,28 @@
+class Key {
+    private signature: number;
+
+    constructor(signature: number) {
+        this.signature = Math.random();
+    }
+    getSignature() {
+        return this.signature;
+    }
+}
+
+class Person{
+    constructor (privat key: new Key)
+    getKey() {
+           return this.key
+       }
+}
+
+abstract class House{
+    door: boolean;
+    key: {};
+    comeln() {
+        this.tenants.push(this.Person)
+    }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
